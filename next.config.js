b318b8087222079635a71d0747a9d0d525be7582
m.tasnimi <m.tasnimi@arz.team>
2025-04-02 04:11:54 +0330

    ci(deploy): update deployment config and permissions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/website' : '',
+  distDir: 'out',
+  basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
   trailingSlash: true,
 
 };
